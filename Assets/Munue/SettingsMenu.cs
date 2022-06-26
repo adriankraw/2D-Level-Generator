5e192041da3c7a4ed77e72bc9b2fe86742282b15
Sandro-Wb <sandro.wb@hotmail.de>
2022-06-26 12:54:41 +0200

    ResolutionDropdown and VolumeSlider
    Fully implemented ResolutionDropdown menu and functional VolumeSlider.

--- Assets/Munue/SettingsMenu.cs
+++ Assets/Munue/SettingsMenu.cs
@@ -1,13 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audiomixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+     void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+      
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) 
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+           if (resolutions[i].width == Screen.currentResolution.width &&
+               resolutions[i].height == Screen.currentResolution.height)
+            {
+                    currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume( float volume)
     {
-        Debug.Log(volume);
+        audiomixer.SetFloat("MasterVolume", volume);
     }
+
+    
 
     public void SetFullscreen (bool isFullscreen)
     {
